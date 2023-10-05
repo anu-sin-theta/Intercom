05bf4a38bdb9c5d25cfbaa41e2a740d6ff5ebce3
Anubhav Singh <audiq4456@gmail.com>
2023-10-05 19:25:09 +0530

    Update theconnector.js

--- theconnector.js
+++ theconnector.js
@@ -1,21 +1,19 @@
 const firebaseConfig = {
     apiKey: "AIzaSyA-V61Tw27Q-2NVfV7i_twZCA3K5N-P_uE",
     authDomain: "csed-intercom.firebaseapp.com",
-    databaseURL: "https://csed-intercom-default-rtdb.asia-southeast1.firebasedatabase.app",
+    databaseURL: "https://csed-intercom.asia-southeast1.firebasedatabase.app",
     projectId: "csed-intercom",
     storageBucket: "csed-intercom.appspot.com",
     messagingSenderId: "597649540949",
     appId: "1:597649540949:web:bfb74efa3e7eed00cf51dc",
     measurementId: "G-0FJGEY4K5S"
 };
-
-// Initialize Firebase
+//dekh bhai inspect karne se kuch nahi hoga tumhe isme se kuch nahi milega source code dekhna hai toh github pe jao @anu-sin-theta.
+//all credential are locked in firebase so you can't access it. reCAPTCHA is also locked so you can't access it.
 firebase.initializeApp(firebaseConfig);
 
-// Get a reference to the Firebase Realtime Database
 const database = firebase.database();
 
-// Add event listeners to buttons
 const allowButton = document.getElementById("allowButton");
 allowButton.addEventListener("click", () => {
     writeToDatabase("Allow");
@@ -31,41 +29,91 @@ viewButton.addEventListener("click", () => {
     writeToDatabase("View");
 });
 
-// Function to write data to the Firebase Realtime Database
 function writeToDatabase(value) {
-    // Write the value to the database
-    database.ref("buttonValue").set(value)
-        .then(() => {
-            console.log("Data written to the database successfully.");
-            showSuccessMessage();
-        })
-        .catch((error) => {
-            console.error("Error writing data to the database:", error);
+    if (value === "View") {
+        database.ref("viewButton").set("View")
+            .then(() => {
+                console.log("Data written to the database successfully.");
+                showSuccessMessage();
+            })
+            .catch((error) => {
+                console.error("Error writing data to the database:", error);
+            });
+    }
+    if (value === "Allow") {
+        database.ref("allowButton").set("Allow")
+            .then(() => {
+                console.log("Data written to the database successfully.");
+                showSuccessMessage();
+            })
+            .catch((error) => {
+                console.error("Error writing data to the database:", error);
+            });
+    }
+    if (value === "Deny") {
+        database.ref("denyButton").set("Deny")
+            .then(() => {
+                console.log("Data written to the database successfully.");
+                showSuccessMessage();
+            })
+            .catch((error) => {
+                console.error("Error writing data to the database:", error);
+            });
+    }
+    setTimeout(function () {
+        removeData();
+    }, 8000);
+}
+//
+
+// Wait for the DOM to be fully loaded
+document.addEventListener("DOMContentLoaded", function() {
+    // Function to read and display door status
+    function readDoorStatus() {
+        const doorStatusElement = document.getElementById("Status"); // Get the status element
+
+        database.ref("doorStatus").on("value", (snapshot) => {
+            const doorStatus = snapshot.val();
+            console.log(doorStatus);
+            if (doorStatus === "Open") {
+                doorStatusElement.innerHTML = "Door is Open";
+                doorStatusElement.style.color = "#4CAF50"; // Optional: Change text color for "Open"
+            } else if (doorStatus === "Closed") {
+                doorStatusElement.innerHTML = "Door is Closed";
+                doorStatusElement.style.color = "#f44336"; // Optional: Change text color for "Closed"
+            } else {
+                doorStatusElement.innerHTML = "Not updated, check R-pi's internet connection :)"; // Handle other status values if needed
+                doorStatusElement.style.color = "#333333"; // Optional: Change text color for other statuses
+            }
         });
+    }
+
+    readDoorStatus();
+});
+
+function refreshPage() {
+    location.reload();
 }
 
-// Function to read data from the Firebase Realtime Database
-function readFromDatabase() {
-    // Read the value from the database
-    database.ref("buttonValue").once("value")
-        .then((snapshot) => {
-            const value = snapshot.val();
-            console.log("Value from the database:", value);
-        })
-        .catch((error) => {
-            console.error("Error reading data from the database:", error);
-        });
+const refreshButton = document.getElementById("refreshButton");
+refreshButton.addEventListener("click", refreshPage);
+
+function removeData() {
+    database.ref("allowButton").remove()
+    database.ref("denyButton").remove()
+    database.ref("viewButton").remove()
 }
 
-// Function to show a success message
+
+
+
 function showSuccessMessage() {
     const successMessage = document.createElement("div");
     successMessage.className = "success";
     successMessage.textContent = "Data written to the database successfully!";
     document.body.appendChild(successMessage);
 
-    // Remove the success message after a few seconds
     setTimeout(() => {
         successMessage.remove();
-    }, 3000); // Remove after 3 seconds
+    }, 3000);
 }
